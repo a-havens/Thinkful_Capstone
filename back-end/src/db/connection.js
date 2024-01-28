9a3e46bb97d3901139a1cf9c458968bfa83a4a39
a-havens <amanda.havens1@yahoo.com>
2024-01-27 21:51:21 -0500

    connection.js junk removal

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -1,9 +1,5 @@
 const environment = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[environment];
 const knex = require("knex")(config);
-
-afterAll(async () => {
-    await knex.destroy();
-  });
   
 module.exports = knex;
